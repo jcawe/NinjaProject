fe1ab5e25717a7465f6dfd7d61c5ca0741ec45d7
jcawe <awelojc@gmail.com>
2018-11-10 12:13:39 +0100

    Closes #8

---
+++ Assets/Scripts/Health/HealthConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Health", menuName="NinjaProject/Health")]
+public class HealthConfiguration : ScriptableObject
+{
+    public int MaxHealth;
+    public int Health;
+}
