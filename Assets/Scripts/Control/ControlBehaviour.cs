2cc8f760952dfdc2ed26e70701d0610f518e24b4
jcawe <awelojc@gmail.com>
2018-11-11 23:48:13 +0100

    Closes #3

---
+++ Assets/Scripts/Control/ControlBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class AxisEvent : UnityEvent<float> { }
+
+public class ControlBehaviour : MonoBehaviour
+{
+    public AxisEvent OnHorizontalAxis;
+    public AxisEvent OnVerticalAxis;
+    public UnityEvent OnSubmit;
+    public UnityEvent OnCancel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        if (x != 0) OnHorizontalAxis?.Invoke(x);
+
+        if (y != 0) OnVerticalAxis?.Invoke(y);
+
+        if (Input.GetButtonDown("Submit")) OnSubmit?.Invoke();
+
+        if (Input.GetButtonDown("Cancel")) OnCancel?.Invoke();
+    }
+}
