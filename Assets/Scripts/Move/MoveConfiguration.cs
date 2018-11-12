768a8f5a54754dad309f6e8d29c8d8067732224d
jmveigac <josemanuelveigacastillo@MacBook-Air-de-Jose.local>
2018-11-12 11:08:14 +0100

    Add Files

---
+++ Assets/Scripts/Move/MoveConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Move", menuName="NinjaProject/Move")]
+public class MoveConfiguration : ScriptableObject
+{
+    public float Speed;
+
+    public Rigidbody RBody;
+}
