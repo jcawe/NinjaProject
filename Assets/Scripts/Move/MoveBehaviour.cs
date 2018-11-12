c7c72f0b26a71e7d08c834c10d3a2ddebbbbd3a9
jmveigac <josemanuelveigacastillo@MacBook-Air-de-Jose.local>
2018-11-12 14:35:59 +0100

    MoveNinja

--- Assets/Scripts/Move/MoveBehaviour.cs
+++ Assets/Scripts/Move/MoveBehaviour.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class MoveBehaviour : MonoBehaviour
 {
     public MoveConfiguration Move;
+    public Rigidbody RBody;
+    
+    public void ApplyMove(float direction)
+    {
+        RBody.AddForce(Vector3.forward * direction * Move.Speed);
+    }
 }
--- Assets/Scripts/Move/MoveConfiguration.cs
+++ Assets/Scripts/Move/MoveConfiguration.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName="Move", menuName="NinjaProject/Move")]
+[CreateAssetMenu(fileName = "Move", menuName = "NinjaProject/Move")]
 public class MoveConfiguration : ScriptableObject
 {
     public float Speed;
-
-    public Rigidbody RBody;
 }
