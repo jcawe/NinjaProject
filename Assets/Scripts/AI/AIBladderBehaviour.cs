6c962e4e295fcdc68a086a98fa0c5c4fc7e62ab2
jcawe <awelojc@gmail.com>
2018-11-19 18:56:49 +0100

    PeeSpot added and visual effect to editor

--- Assets/Scripts/AI/AIBladderBehaviour.cs
+++ Assets/Scripts/AI/AIBladderBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
@@ -7,13 +8,13 @@ using UnityEngine.Events;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIBladderBehaviour : MonoBehaviour
 {
-    public Transform WC;
     public float minWCDistance;
     public UnityEvent OnPeeingStart;
     public UnityEvent OnPeeingFinish;
     private NavMeshAgent agent;
     private bool peeing;
     private bool haveToPeeing;
+    Vector3 peeSpot;
 
     void Start()
     {
@@ -24,7 +25,7 @@ public class AIBladderBehaviour : MonoBehaviour
     {
         if(!haveToPeeing || peeing) return;
         
-        var dist = Mathf.Abs((transform.position - WC.position).magnitude);
+        var dist = Mathf.Abs((transform.position - peeSpot).magnitude);
         if(dist < minWCDistance) StartCoroutine("Peeing");
     }
 
@@ -32,8 +33,28 @@ public class AIBladderBehaviour : MonoBehaviour
     {
         OnPeeingStart?.Invoke();
         Debug.Log("Goin to WC");
-        agent.SetDestination(WC.position);
+        peeSpot = FindNearestPeeSpot();
+        agent.SetDestination(peeSpot);
         haveToPeeing = true;
+    }
+
+    Vector3 FindNearestPeeSpot()
+    {
+        var peeSpots = Object.FindObjectsOfType<PeeSpotBehaviour>().Select(x => x.transform.position);
+
+        Vector3 result = Vector3.zero;
+        float dist = -1f;
+        foreach(var spot in peeSpots)
+        {
+            var spotDist = Vector3.Distance(transform.position, spot);
+            if(dist < 0 || dist > spotDist)
+            {
+                dist = spotDist;
+                result = spot;
+            }
+        }
+
+        return result;
     }
 
     IEnumerator Peeing()
--- Assets/Scripts/Bladder/BladderBehaviour.cs
+++ Assets/Scripts/Bladder/BladderBehaviour.cs
@@ -17,11 +17,11 @@ public class BladderBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bladder += 1f * Time.deltaTime;
+        bladder += Profile.Speed * Time.deltaTime;
 
-        bladder = Mathf.Clamp(bladder, 0f, 10f);
+        bladder = Mathf.Clamp(bladder, 0f, Profile.Max);
 
-        if(bladder == 10f && !notified) 
+        if(bladder == Profile.Max && !notified) 
         {
             notified = true;
             OnBladderFilled?.Invoke();
@@ -34,5 +34,17 @@ public class BladderBehaviour : MonoBehaviour
         bladder = 0f;
         Debug.Log("Bladder empty!");
         notified = false;
+    }
+
+    [ExecuteInEditMode]
+    void OnDrawGizmos()
+    {
+        var perc = bladder / (Profile?.Max ?? 1f);
+
+        Gizmos.color = perc == 1 ? Color.red : Color.cyan;
+        
+        Gizmos.DrawWireSphere(transform.position + Vector3.up*2f, 0.2f);
+        Gizmos.DrawSphere(transform.position + Vector3.up*2f,
+             perc * 0.2f);
     }
 }
