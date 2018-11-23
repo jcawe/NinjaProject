c0d0ab6f154437f8c715460e2ed42fe48659450c
Jose Antonio Cáceres Romero <awelojc@gmail.com>
2018-11-23 12:20:56 +0100

    Move improvement (#16)
    * AI now uses MoveBehaviour and movement now uses correctly the animator
    
    * Prefab rename

--- Assets/Scripts/AI/PursuitAIBehaviour.cs
+++ Assets/Scripts/AI/PursuitAIBehaviour.cs
@@ -9,18 +9,15 @@ public class PursuitAIBehaviour : MonoBehaviour
 {
     public UnityEvent OnTargetLost;
     public GameObject TargetPrefab;
-    public float RunMultiplier;
     public float SearchTime;
 
     NavMeshAgent agent;
     bool searching;
     GameObject target;
-    float walkSpeed;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        walkSpeed = agent.speed;
     }
 
     public void Spotted(GameObject target)
@@ -28,13 +25,13 @@ public class PursuitAIBehaviour : MonoBehaviour
         if(this.target) Destroy(this.target);
         this.target = Instantiate(TargetPrefab, target.transform.position, target.transform.rotation);
         agent.SetDestination(this.target.transform.position);
-        agent.speed = walkSpeed * RunMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target && !searching && !agent.hasPath) StartCoroutine(Search());
+        if(target && !searching 
+            && agent.remainingDistance <= agent.stoppingDistance) StartCoroutine(Search());
     }
 
     IEnumerator Search()
@@ -43,7 +40,6 @@ public class PursuitAIBehaviour : MonoBehaviour
         yield return new WaitForSeconds(SearchTime);
         Destroy(target);
         searching = false;
-        agent.speed = walkSpeed;
         OnTargetLost?.Invoke();
     }
 }
